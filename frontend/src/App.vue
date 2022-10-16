dbff789f9d4ec66a4f984ba0b9ee2ad93094b9fc
EJK <ErikJohannesKostic@gmail.com>
2022-10-16 16:49:33 +0200

    seems to work correctly

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,13 +1,40 @@
 <template>
   <div id="app" class="font-mono">
-    <Navbar/>
-    <router-view/>
-    <Footer/>
+
+    <Navbar />
+    <router-view />
+    <div>
+      <Chart class="w-96" type="pie" :data="chartData" :options="lightOptions" />
+    </div>
+    <Footer />
   </div>
 </template>
 
 <script>
 export default {
-  name: "App"
+  name: "App",
+  data() {
+    return {
+      chartData: {
+        labels: ['A', 'B', 'C'],
+        datasets: [
+          {
+            data: [300, 50, 100],
+            backgroundColor: ["#42A5F5", "#66BB6A", "#FFA726"],
+            hoverBackgroundColor: ["#64B5F6", "#81C784", "#FFB74D"]
+          }
+        ]
+      },
+      lightOptions: {
+        plugins: {
+          legend: {
+            labels: {
+              color: '#495057'
+            }
+          }
+        }
+      }
+    }
+  }
 };
 </script>
